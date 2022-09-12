b3382f37be6b76d6a33ae0d048c00d869dd33bcc
k.dabrock <k.dabrock@fz-juelich.de>
2022-09-12 16:28:37 +0200

    fix query url

--- src/builda_client/client.py
+++ src/builda_client/client.py
@@ -623,7 +623,7 @@ class ApiClient:
             statistics.append(statistic)
         return statistics
 
-    def get_energy_consumption_statistics(self,country: str = '', nuts_level: int | None = None, nuts_code: str | None = None) -> list[EnergyConsumptionStatistics]:
+    def get_energy_consumption_statistics(self, country: str = '', nuts_level: int | None = None, nuts_code: str | None = None) -> list[EnergyConsumptionStatistics]:
         """Get the energy consumption statistics for the given nuts level or nuts code. Only one of nuts_level and nuts_code may be specified.
 
         Args:
@@ -645,9 +645,9 @@ class ApiClient:
         
         query_params = f"?country={country}"
         if nuts_level is not None:
-            query_params = f"?nuts_level={nuts_level}"
+            query_params = f"&nuts_level={nuts_level}"
         elif nuts_code is not None:
-            query_params = f"?nuts_code={nuts_code}"
+            query_params = f"&nuts_code={nuts_code}"
 
         url: str = f"""{self.base_url}{self.ENERGY_STATISTICS_URL}{query_params}"""
         try:
